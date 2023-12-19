486685b8140dfa18e09fb2b3677c7d1a601d5fa7
JanNautalis <filipkedzior@gmail.com>
2023-12-19 15:33:53 +0100

    zmiana w app.py routingu bo nie odpalało bez myCalendar

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-import myCalendar
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    calendar_template = myCalendar.showCalendar(2023, 5, [1,4])
-    return render_template('index.html.jinja', calendar_template=calendar_template)
+    return render_template('index.html.jinja')
 
 if __name__ == '__main__':
    app.run(debug=True)
